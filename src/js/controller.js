6ed2d92f1b47caec690014151dc6a2a4ba97dab6
ThijsSoleoMedia <thijs@soleomedia.com>
2022-04-06 10:43:11 +0200

    Updated results view

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,14 @@
 import * as model from './model';
 import recipeView from './views/recipeView';
 import searchView from './views/searchView';
+import resultsView from './views/resultsView';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -24,10 +29,17 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
+    // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
 
+    // 2) Load search results
     await model.loadSearchResults(query);
+
+    // 3) Render results
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.error(err);
   }
